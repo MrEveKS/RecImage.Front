bf25904aa2ef89a39bae09f11f8f2d2af4095f94
Evgeniy Klimov <vikkes@yandex.ru>
2020-12-10 20:14:17 +0700

    add enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+if (environment.production) {
+    enableProdMode();
+}
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
