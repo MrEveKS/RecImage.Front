5aa5dff430c57d8ec5b4fe3283ffda7b6f812548
Evgeniy Klimov <vikkes@yandex.ru>
2021-02-05 17:02:48 +0700

    hide footer-select for coloring navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
     selector: 'main-app',
@@ -9,6 +12,16 @@ export class AppComponent {
 
     public loading: boolean;
     public showPreview = true;
+
+    public hideFooterSelect$: Observable<boolean>;
+
+    public constructor(router: Router) {
+        this.hideFooterSelect$ = router.events
+            .pipe(
+                filter((event: RouterEvent) => event instanceof NavigationEnd),
+                map((data: NavigationEnd) => data.url.indexOf('coloring') !== -1)
+            );
+    }
 
     public handleBack(): void {
         this.showPreview = true;
