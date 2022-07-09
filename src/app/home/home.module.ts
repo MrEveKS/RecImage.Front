305d594db7765c3e094bd82d38172a867d4c60a5
Evgenii Klimov <vikkes@yandex.ru>
2022-07-09 18:39:50 +0300

    update buttons on main page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 
-import { ImageCollectionModule } from '../image-collection/image-collection.module';
-
 import { HomeComponent } from './home.component';
 
+import { ImageCollectionModule } from '../image-collection/image-collection.module';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, ImageCollectionModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ImageCollectionModule,
+    MatButtonModule,
+  ],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
